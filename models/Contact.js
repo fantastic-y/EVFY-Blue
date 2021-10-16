fd054fb2a427d62e08d3c3e388a7f8a92e9d3e53
Fupin Yao <xcmax@qq.com>
2021-10-16 09:32:21 -0700

    added db schemas

---
+++ models/Contact.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    trim: true,
+  },
+  last_name: {
+    type: String,
+    trim: true,
+  },
+  street: {
+    type: String,
+    trim: true,
+  },
+  city: {
+    type: String,
+    trim: true,
+  },
+  zip: {
+    type: Number,
+    trim: true,
+  },
+  phone: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+  }
+});
+
+module.exports = mongoose.model("Contact", schema);
