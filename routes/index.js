f702435616751d48d2f442ab583bb28e010b28b8
Vishnu <vprabh@pdisoftware.com>
2021-10-16 20:51:29 -0500

    contact done

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const passport =  require("passport");
-const LocalStrategy =  require("passport-local");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 const path = require("path");
 const auth = require("http-auth");
 const bcrypt = require("bcrypt");
-const bodyParser =  require("body-parser");
+const bodyParser = require("body-parser");
 const { check, validationResult } = require("express-validator");
-const User =  require("../models/user");
+const User = require("../models/user");
 const Contact = require("../models/contact");
 const router = express.Router();
 const basic = auth.basic({
     file: path.join(__dirname, "../users.htpasswd"),
 });
-const app =  express();
-const expSession = require("express-session") ({
-    secret:"mysecret",       //decode or encode session
-    resave: false,          
-    saveUninitialized:true,
-    cookie:{
+const app = express();
+
+const expSession = require("express-session")({
+    secret: "mysecret", //decode or encode session
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
         httpOnly: true,
         secure: true,
-        maxAge: 1*60*1000
-    }    
+        maxAge: 1 * 60 * 1000,
+    },
 });
 
-passport.serializeUser(User.serializeUser());       //session encoding
-passport.deserializeUser(User.deserializeUser());   //session decoding
+passport.serializeUser(User.serializeUser()); //session encoding
+passport.deserializeUser(User.deserializeUser()); //session decoding
 passport.use(new LocalStrategy(User.authenticate()));
-app.use(bodyParser.urlencoded(
-      { extended:true }
-))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(expSession);
 app.use(passport.initialize());
 app.use(passport.session());
@@ -72,11 +73,14 @@ router.get("/login", (req, res) => {
     });
 });
 
-app.post("/login",passport.authenticate("local",{
-    successRedirect:"/userprofile",
-    failureRedirect:"/login"
-}),function (req, res){
-});
+app.post(
+    "/login",
+    passport.authenticate("local", {
+        successRedirect: "/userprofile",
+        failureRedirect: "/login",
+    }),
+    function (req, res) {}
+);
 
 router.get(
     "/register",
@@ -102,39 +106,45 @@ router.post(
             .withMessage("Please enter an password"),
     ],
     async (req, res) => {
-        User.register(new User({username: req.body.username}), req.body.password, function(err,user){
-            if(err){
-                console.log(err);
-                res.render("register");
+        User.register(
+            new User({ username: req.body.username }),
+            req.body.password,
+            function (err, user) {
+                if (err) {
+                    console.log(err);
+                    res.render("register");
+                }
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/login");
+                });
             }
-            passport.authenticate("local")(req,res,function(){
-                res.redirect("/login");
-            })
-        })
+        );
     }
 );
 
 router.post(
-    "/contact", 
+    "/savecontact",
     [
         check("email")
             .isLength({
                 min: 1,
             })
-            .withMessage("Please enter your email")
+            .withMessage("Please enter your email"),
     ],
-    (req, res)=>{
+    (req, res) => {
         const contact = new Contact(req.body);
+        console.log(req.body);
         contact
             .save()
             .then(() => {
-                res.render("success", {
-                    title: "Your contact form is recorded! We will contact you later!",
+                res.json({
+                    contact,
                 });
             })
             .catch((err) => {
                 console.log(err);
                 res.send("Sorry! Something went wrong.");
             });
-    })
+    }
+);
 module.exports = router;
