823032286afa14a622de8f7b90b7f6b1e59cf150
Fan <52841919+fantastic-y@users.noreply.github.com>
2021-10-23 10:37:35 -0700

    add the captcha for contact form

---
+++ public/js/captcha.js
@@ -0,0 +1,53 @@
+/* ---- random codes ---- */
+let code = ' '; //initialize to null value
+let getCode = ' '; //to store entered code
+//function to generate combination of characters
+function generateCode() {
+    //create variables to store generated codes and the type of characters we want to show as codes
+    
+    let btnvalue; //for button boolean value
+    //create variable to hold the type of characters we want to show as codes
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+    //generate character multiple times using a loop
+    for (i = 1; i <=4; i++) {
+        let char = Math.random()* str.length; //random select a character from the variable and then store in a new variable
+        code += str.charAt(char) //accumulate the generated character into a string of 8 characters
+    }
+    return code; //return the final accumulated string when loop ends
+}
+
+//get html element to display
+document.getElementById("captcha-value").innerHTML = generateCode();
+
+//disable button
+function disableButton(btnvalue) {
+    document.getElementById("formsubmit").disabled = btnvalue; //able/disable button
+    if (btnvalue == true) {
+        //by default, it is disabled set button and label color translucent
+        document.getElementById("formsubmit").style.backgroundColor = "rgba(140, 198, 63, 0.3)";
+        document.getElementById("formsubmit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        //active it,set button and label color with no transparency
+        document.getElementById("formsubmit").style.backgroundColor = "rgba(140, 198, 63, 1)";
+        document.getElementById("formsubmit").style.color = "rgba(255, 255, 255, 1)";
+    }
+}
+
+//listen to user input code
+let codebox = document.getElementById("entered-code"); //get textbox
+codebox.addEventListener("input", evaluateCode); //listen to code entered in textbox
+
+//run function if detected user had entered a character in textbox
+function evaluateCode() {
+    getCode = document.getElementById("entered-code").value; //get character entered
+    let charset1 = getCode.trim(); //remove any hidden characters entered
+    let charset2 = code.trim(); //remove any hidden characters generated
+    //test if code entered matches the number of genereated characters
+    if (charset1.length == charset2.length && charset1 == charset2) {
+        disableButton(false); //if match, run the function to enable button
+    }
+}
+
+//activate function
+disableButton();
