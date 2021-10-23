79e910f551105ba5f841053b344dbcb86673f6d3
Fupin Yao <xcmax@qq.com>
2021-10-23 11:52:43 -0700

    es6

--- public/js/return_to_top.js
+++ public/js/return_to_top.js
@@ -2,9 +2,7 @@
 mybutton = document.getElementById("returnBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
+window.onscroll = () =>  {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
